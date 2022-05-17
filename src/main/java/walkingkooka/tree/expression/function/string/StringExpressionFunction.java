5b1dfa2ca43b1b9f86fd688df4b525f67f97ac81
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2022-05-17 22:41:08 +1000

    https://github.com/mP1/walkingkooka-tree/pull/510 ExpressionFunctionKind.CONVERT_PARAMETERS
    - https://github.com/mP1/walkingkooka-tree/pull/510
    - ExpressionFunctionKind.CONVERT_PARAMETERS

--- src/main/java/walkingkooka/tree/expression/function/string/StringExpressionFunction.java
+++ src/main/java/walkingkooka/tree/expression/function/string/StringExpressionFunction.java
@@ -68,6 +68,7 @@ abstract class StringExpressionFunction<C extends ExpressionFunctionContext> imp
     }
 
     private final Set<ExpressionFunctionKind> KINDS = EnumSet.of(
+            ExpressionFunctionKind.CONVERT_PARAMETERS,
             ExpressionFunctionKind.EVALUATE_PARAMETERS,
             ExpressionFunctionKind.RESOLVE_REFERENCES
     );
